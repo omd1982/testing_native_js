ecdbf446ec8df82d60ba248676b4ca120d7f6c4e
Alexandr1982 <omd1982@yandex.by>
2022-02-24 22:00:13 +0300

    error: pathspec 'the' did not match any file(s) known to git

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+let num
+console.log(num || 24) //24    
+console.log(100 && 24) //24     
+console.log(num && 24) //undefined 
+*/
+
+
+let arr = []
+for (var i = 0; i < 10; i++){     //если var  - то не работает!
+  arr.push(function(){console.log(i)})
+}
+arr[5]() // - если var, то получаем все время 10 индекс при
+// обращении к любому элементу
+
+let arr2 = []
+for ( var i = 0; i < 10; i++){     //если var  -  так работает!
+ (function(j){
+  arr2.push(function(){console.log(j)})
+ })(i)
+}
+arr2[6]()
